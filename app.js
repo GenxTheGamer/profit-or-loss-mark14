8f6658fccc24be2f7383c8f912d096e13a75b1b6
Vaibhav Vyas <52570863+GenxTheGamer@users.noreply.github.com>
2021-09-28 18:14:56 +0530

    initialized inputs and outputs. Added wiring to them from app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+const initialPrice = document.querySelector("#initial-price");
+const quantity = document.querySelector("#quantity");
+const currentPrice = document.querySelector("#current-price");
+const submitBtn = document.querySelector("#submit-button");
+const output = document.querySelector("#output");
